5d8cec75021d273a26f7a7ce4484f9bffb6385fe
Erfan esmaeilzade <erfanesmeilzade@gmail.com>
2020-09-27 00:00:52 +0330

    rgb function add to project

---
+++ rgb/rgb.c
@@ -0,0 +1,27 @@
+/*
+ * rgb.c
+ *
+ * Created: 29/03/2020 07:53:34 È.Ù
+ * Author: MKH
+ */
+
+#include <io.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+void main(void)
+{
+DDRD.2=1;
+DDRD.3=1;
+DDRD.4=1;
+PORTD.2=0;
+PORTD.3=1;
+PORTD.4=1;
+while (1)
+    {
+        PORTD.2=1;
+        PORTD.3=1;
+        PORTD.4=0;    
+    }
+}
